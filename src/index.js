ac746ae3b523e8a2899920a2028da9908cac6178
tirthamoulib <btirthamouli@gmail.com>
2021-10-30 01:59:45 +0530

    Added basic example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('HERE');
+import { BinarySearchTreeNode, drawBinaryTree, VisualizationType } from 'binary-tree-visualizer';
+
+const root = new BinarySearchTreeNode(100);
+[50, 145, 150, 130, 120, 140, 30, 70, 75].forEach((num) => root.insert(num));
+
+drawBinaryTree(root, document.querySelector('canvas'), {
+  type: VisualizationType.EXPANDABLE,
+});
