463df662d1d42e6d8838dc4ea7e3b79268ec4652
tirthamoulib <btirthamouli@gmail.com>
2021-11-22 22:48:29 +0530

    Updated for HIGHLIGHT tree

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const root = new BinarySearchTreeNode(100);
 [50, 145, 150, 130, 120, 140, 30, 70, 75].forEach((num) => root.insert(num));
 
 drawBinaryTree(root, document.querySelector('canvas'), {
-  type: VisualizationType.EXPANDABLE,
+  type: VisualizationType.HIGHLIGHT,
 });
